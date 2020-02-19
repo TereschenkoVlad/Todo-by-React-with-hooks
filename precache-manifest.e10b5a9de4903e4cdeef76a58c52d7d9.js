eb350e5f71ae5c207a250785ff9b904c8bb6f384
Vlad Tereshchenko <vlad.tereshenko@ekreative.com>
2020-02-19 18:33:05 +0200

    Updates

---
+++ precache-manifest.e10b5a9de4903e4cdeef76a58c52d7d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a44205a8b3641d9af232bec5a00e44f",
+    "url": "/Todo-by-React-with-hooks/index.html"
+  },
+  {
+    "revision": "a25dd6fa509e43f2d450",
+    "url": "/Todo-by-React-with-hooks/static/css/main.57cc5ab1.chunk.css"
+  },
+  {
+    "revision": "56b883f09b96ac5ec799",
+    "url": "/Todo-by-React-with-hooks/static/js/2.11d12b7e.chunk.js"
+  },
+  {
+    "revision": "a25dd6fa509e43f2d450",
+    "url": "/Todo-by-React-with-hooks/static/js/main.b1c85488.chunk.js"
+  },
+  {
+    "revision": "756b082a5b853ad845ee",
+    "url": "/Todo-by-React-with-hooks/static/js/runtime~main.2b4705ec.js"
+  }
+]);
