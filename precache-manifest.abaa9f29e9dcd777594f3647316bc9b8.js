6c9ffebe5c28b0e7b54d589b53f21eb9c8c126e5
Vlad Tereshchenko <vlad.tereshenko@ekreative.com>
2020-02-21 11:33:48 +0200

    Updates

---
+++ precache-manifest.abaa9f29e9dcd777594f3647316bc9b8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd382c00bdc870946c19eb3ea18dc4c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d2dda6ab1e13d271099",
+    "url": "/static/css/main.3d709492.chunk.css"
+  },
+  {
+    "revision": "92f0a7d2ba4ac612ca70",
+    "url": "/static/js/2.27f4da6d.chunk.js"
+  },
+  {
+    "revision": "3d2dda6ab1e13d271099",
+    "url": "/static/js/main.aceb0fee.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
